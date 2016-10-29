ef3ee6e4625667c69a6be8f17dc7a998bfcae449
Zhi-Wei <ZhiWei.Lu@outlook.com>
2016-10-29 17:04:21 +0800

    新建與註冊 CORSResponseFilter。

--- RssReaderWebApi/src/main/java/tw/edu/ntou/summerHomework/RssReaderWebApi/appStart/AppStart.java
+++ RssReaderWebApi/src/main/java/tw/edu/ntou/summerHomework/RssReaderWebApi/appStart/AppStart.java
@@ -3,9 +3,12 @@ package tw.edu.ntou.summerHomework.RssReaderWebApi.appStart;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import tw.edu.ntou.summerHomework.RssReaderWebApi.filters.CORSResponseFilter;
+
 @ApplicationPath("RssReader")
 public class AppStart extends ResourceConfig {
 	public AppStart() {
 		packages("tw.edu.ntou.summerHomework.RssReaderWebApi");
+		register(CORSResponseFilter.class);
 	}
 }
